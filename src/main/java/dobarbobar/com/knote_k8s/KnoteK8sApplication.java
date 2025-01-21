30ffff0f063fa3a9398a33d9aa1c33ee27a819c4
Spas Kolaksazov <spas.kolaksazov@gmail.com>
2025-01-21 18:50:38 +0100

    adding spring boot source

---
+++ src/main/java/dobarbobar/com/knote_k8s/KnoteK8sApplication.java
@@ -0,0 +1,13 @@
+package dobarbobar.com.knote_k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnoteK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnoteK8sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dobarbobar/com/knote_k8s/KnoteK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package dobarbobar.com.knote_k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnoteK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
